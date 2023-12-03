4a9cd9da291b37ee05849ed49eca19d30305ce58
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-03 18:27:27 -0300

    feat01: implementa os DTOs `WorkoutDto` e `WorkoutCreationDto`

---
+++ src/main/java/com/betrybe/fitness/dto/WorkoutCreationDto.java
@@ -0,0 +1,6 @@
+package com.betrybe.fitness.dto;
+
+/**
+ * The type Workout creation dto.
+ */
+public record WorkoutCreationDto(String name, Integer repetitions, String secretTechnique) {}
---
+++ src/main/java/com/betrybe/fitness/dto/WorkoutDto.java
@@ -0,0 +1,6 @@
+package com.betrybe.fitness.dto;
+
+/**
+ * The type Workout dto.
+ */
+public record WorkoutDto(Long id, String name, Integer repetitions) {}
