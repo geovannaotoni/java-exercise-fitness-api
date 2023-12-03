3a9d2c9cee6ada08c5ef1fec99af21832212708f
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-03 20:27:04 -0300

    feat08: cria a rota POST /fitness/workouts

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,13 +1,17 @@
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessService;
 import com.betrybe.fitness.service.FitnessServiceInterface;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +50,17 @@ public class FitnessController implements FitnessControllerInterface {
 
     // retorna status 200 (ok) com o DTO no corpo da resposta
     return ResponseEntity.ok(workoutDto.get());
+  }
+
+  /**
+   * Create workout response entity.
+   *
+   * @param newWorkout the workout creation dto
+   * @return the response entity
+   */
+  @PostMapping("/workouts")
+  public ResponseEntity<WorkoutDto> createWorkout(@RequestBody WorkoutCreationDto newWorkout) {
+    WorkoutDto newWorkoutDto = service.saveWorkout(newWorkout);
+    return ResponseEntity.status(HttpStatus.CREATED).body(newWorkoutDto);
   }
 }
