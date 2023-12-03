acb5ef9971abec9853b59eba43d0da7363a0c16e
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-03 19:15:56 -0300

    feat04: cria a rota GET /fitness

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -2,6 +2,7 @@ package com.betrybe.fitness.controller;
 
 import com.betrybe.fitness.service.FitnessService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class FitnessController implements FitnessControllerInterface {
   @Autowired
   public FitnessController(FitnessService service) {
     this.service = service;
+  }
+
+  @GetMapping
+  public String getRoot() {
+    return "Boas vindas à API de Fitness!";
   }
 }
