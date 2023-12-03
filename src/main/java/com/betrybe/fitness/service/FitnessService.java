d0c5000071ed0f95982db7344e5da82fa9b08a49
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-03 20:31:25 -0300

    feat09: implementa o método getAllWorkouts da camada de serviço

--- src/main/java/com/betrybe/fitness/service/FitnessService.java
+++ src/main/java/com/betrybe/fitness/service/FitnessService.java
@@ -4,6 +4,7 @@ import com.betrybe.fitness.database.FakeFitnessDatabase;
 import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.model.Workout;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,6 +59,15 @@ public class FitnessService implements FitnessServiceInterface {
 
   @Override
   public List<WorkoutDto> getAllWorkouts() {
-    return null;
+    List<WorkoutDto> allWorkouts = new ArrayList<>();
+
+    for (Workout workout : database.getAllWorkouts()) {
+      allWorkouts.add(new WorkoutDto(
+          workout.getId(),
+          workout.getName(),
+          workout.getRepetitions()
+      ));
+    }
+    return allWorkouts;
   }
 }
